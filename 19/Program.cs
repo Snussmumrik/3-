e21af3f193b5242f06edf24fda0e7839c1aede0e
Marina <PyatkovaM@yandex.ru>
2023-06-20 15:03:56 +0300

    решение 19 задачи

---
+++ 19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Ввведите пятизначное число: ");
+string userInput = Console.ReadLine() ?? "";
+int num = int.Parse(userInput);
+
+int a = num / 10000;
+int b = num % 10;
+
+Console.WriteLine ($" первая цифра --> {a}, последняя цифра --> {b} ");
+
+int c = (num % 10000) / 1000;
+int d = (num % 100) /10;
+
+Console.WriteLine ($" вторая цифра --> {c}, четвертая цифра --> {d} ");
+
+if ( a==b && c==d )
+{
+     Console.WriteLine($" {num} --> палиндром ");
+}
+else
+{
+     Console.WriteLine($" {num} --> не палиндром ");
+}
