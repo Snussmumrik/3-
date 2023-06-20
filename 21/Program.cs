61238132f3dc7c87dff44d863daa110790c77e67
Marina <PyatkovaM@yandex.ru>
2023-06-20 15:28:06 +0300

    решение 21 задачи

---
+++ 21/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите x1: ");
+int numX1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите у1: ");
+int numY1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z1: ");
+int numZ1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите x2: ");
+int numX2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите у2: ");
+int numY2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z2: ");
+int numZ2 = int.Parse(Console.ReadLine());
+
+int distance = (numX1 - numX2)*(numX1 - numX2) + (numY1 - numY2)*(numY1 - numY2) + (numZ1 - numZ2)*(numZ1 - numZ2);
+
+Console.WriteLine($" расстояние между двумя точками --> {distance} ");
