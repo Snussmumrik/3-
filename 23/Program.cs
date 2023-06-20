c435334b63cbd10e6574b3688024828fc0add4b1
Marina <PyatkovaM@yandex.ru>
2023-06-20 15:55:40 +0300

    решение 23 задачи

---
+++ 23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine ());
+int b = 0;
+
+Console.WriteLine($" {a} ---> "); 
+
+while (b != a - 1)
+{
+     b ++;
+     Console.WriteLine($" {b * b * b},  ");   
+}
+b ++;
+Console.WriteLine($" {b * b * b}.");   
